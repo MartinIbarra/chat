3987ffd35e2cc838d586c85d8042b7d6b389ee13
Martin Ibarra <oso.caballito@hotmail.com>
2015-03-02 19:50:22 -0300

    moongose y parser's

--- app.js
+++ app.js
@@ -2,14 +2,32 @@
 var express = require('express'),
     app = express(),
     jade = require('jade'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
     server = require('http').createServer(app),
     path = require('path'),
     io = require('socket.io').listen(server),
     port = 80;
 
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    name: String,
+    provider:String,
+    provider_id:{type: String, unique:true },
+    photo:String,
+    createdAt:{type: Date, defoult: Date.now}
+});
+
+var User = mongoose.model('User', UserSchema);
+
 server.listen(port, function() {
     console.log('server corriendo en el puerto: ' + port);
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
