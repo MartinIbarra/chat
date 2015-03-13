916eb240f8d469ac29d14fad7a7de3ebe8260e6e
Martin Ibarra <oso.caballito@hotmail.com>
2015-03-13 17:18:32 -0300

    lol api

--- app.js
+++ app.js
@@ -2,37 +2,61 @@
 var express = require('express'),
     app = express(),
     jade = require('jade'),
+    request = require('request'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     server = require('http').createServer(app),
-    path = require('path'),
-    io = require('socket.io').listen(server),
+    path = require('path');
+
+var io = require('socket.io').listen(server),
     port = 80;
+
+var fs = require('fs');
+
+var request = require('request');
+
+var session = require('express-session'),
+    RedisStore = require('connect-redis')(session);
 
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+//modelo usuario de mongodb
 var UserSchema = new Schema({
     name: String,
-    provider:String,
-    provider_id:{type: String, unique:true },
-    photo:String,
-    createdAt:{type: Date, defoult: Date.now}
+    provider: String,
+    provider_id: {
+        type: String,
+        unique: true
+    },
+    photo: String,
+    createdAt: {
+        type: Date,
+        defoult: Date.now
+    }
 });
 
 var User = mongoose.model('User', UserSchema);
 
-server.listen(port, function() {
-    console.log('server corriendo en el puerto: ' + port);
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+//seteamos el modelo de template
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+
+//middlewares de session y parse
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+//app.use(session({secret: "hola", cookie: 60000}));
+
 app.get("/", function(req, res) {
     res.render("home");
+});
+
+app.get('/:user', function(req, res) {
+
 });
 
 io.on('connection', function(socket) {
@@ -44,11 +68,39 @@ io.on('connection', function(socket) {
         });
     });
 
+    socket.on('nombreUsuario', function(data) {
+        var key;
+        fs.readFile('../lolApiKey/apikey.txt', function(err, res) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                key = res;
+                console.log(key.toString());
+                //hacemos el request y leemos la key en otro archivo
+                request('https://' + data.region + '.api.pvp.net/api/lol/las/v1.4/summoner/by-name/' + data.nombre + '?api_key=' + key.toString(), function(err, res, body) {
+                    if (!err && res.statusCode === 200) {
+                        socket.emit('user', body);
+                    } else {
+                        return err;
+                    }
+                });
+            }
+        });
+    });
+
     socket.on('disconect', function(data) {
-        console.log(data.toArray());
+
+        console.log(data.toString());
+
         io.emit('userDisconected', {
             user: data.id
         });
+
         console.log('usuario ' + data.id);
     });
+});
+
+server.listen(port, function() {
+    console.log('server corriendo en el puerto: ' + port);
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,12 +1,29 @@
 <script>
 "use strict";
 var socket = io.connect();
-console.log(socket);
+$(document).ready(function(){
+	console.log(socket);
+});
 
-$('form').submit(function() {
-    socket.emit('mjs', $('#m').val());
-    $('#m').val('');
-    return false;
+$('#log-in').submit(function (e){
+	e.preventDefault();
+	socket.emit('nombreUsuario', {
+		nombre: $('#nombre').val(),
+		region: $('#region_text').val()
+	});
+
+	socket.on('user',function (data){
+		console.log('nombre: '+ data);
+	});
+	socket.on('champions',function (data){
+		console.log('champions: '+ data);
+	});
+});
+
+$('#chat').submit(function (e) {
+	e.preventDefault();
+    socket.emit('mjs', $('#chat_text').val());
+    $('#chat_text').val('');
 });
 socket.on('mjsBroadcast', function(req) {
     $('#messages').append($('<li>').text(req.mensaje));
@@ -15,7 +32,8 @@ socket.on('userDisconected', function(data) {
     console.log(data);
 });
 
-window.URL = window.URL || window.webkitURL;
+//future videoCalling
+/*window.URL = window.URL || window.webkitURL;
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
 navigator.getUserMedia({
@@ -25,5 +43,5 @@ navigator.getUserMedia({
     document.querySelector('video').src = window.URL.createObjectURL(vid);
 }, function(err) {
     console.log(err);
-});
+});*/
 </script>
